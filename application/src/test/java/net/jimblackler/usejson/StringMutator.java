d7377f982f5c140d62d00f0f7751992c54ac4e70
Jim Blackler <jimblackler@google.com>
2020-10-19 15:43:22 +0100

    Improvements to String Mutator.

--- application/src/test/java/net/jimblackler/usejson/StringMutator.java
+++ application/src/test/java/net/jimblackler/usejson/StringMutator.java
@@ -3,11 +3,18 @@ package net.jimblackler.usejson;
 import java.util.Random;
 
 public class StringMutator {
+  static final String IMPORTANT_CHARACTERS = "[]{}\"':.,\\/*\n-e";
   public static String mutate(String content, Random random) {
     StringBuilder builder = new StringBuilder(content);
     do {
-      builder.setCharAt(random.nextInt(builder.length()), (char) random.nextInt());
-    } while (random.nextFloat() < 0.75f);
+      char ch;
+      if (random.nextFloat() < 0.5f) {
+        ch = (char) random.nextInt();
+      } else {
+        ch = IMPORTANT_CHARACTERS.charAt(random.nextInt(IMPORTANT_CHARACTERS.length()));
+      }
+      builder.setCharAt(random.nextInt(builder.length()), ch);
+    } while (random.nextFloat() < 0.99f);
 
     return builder.toString();
   }
