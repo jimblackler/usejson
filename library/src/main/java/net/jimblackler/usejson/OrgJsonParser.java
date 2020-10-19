30ff7025f874934582e389053c6e5a1a5d514f8b
Jim Blackler <jimblackler@google.com>
2020-10-19 23:41:36 +0100

    Minor format fix.

--- library/src/main/java/net/jimblackler/usejson/OrgJsonParser.java
+++ library/src/main/java/net/jimblackler/usejson/OrgJsonParser.java
@@ -1,7 +1,6 @@
 package net.jimblackler.usejson;
 
 public class OrgJsonParser {
-
   /**
    * Converts a JSON5 or a JSON document into a org.json.JSONObject, an org.json.JSONArray, or in
    * the case of a sole value, null, a String, a Number or a Boolean.
