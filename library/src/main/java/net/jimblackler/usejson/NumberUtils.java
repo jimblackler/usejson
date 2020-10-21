503337de10d7d37c1406be9fdd7312ef185ea113
Jim Blackler <jimblackler@google.com>
2020-10-22 00:05:37 +0100

    Silencing warnings by renaming parameter.

--- library/src/main/java/net/jimblackler/usejson/NumberUtils.java
+++ library/src/main/java/net/jimblackler/usejson/NumberUtils.java
@@ -6,19 +6,19 @@ public class NumberUtils {
   public static Object toBestObject(BigInteger bigInteger) {
     try {
       return bigInteger.byteValueExact();
-    } catch (ArithmeticException ex) {
+    } catch (ArithmeticException ignored) {
     }
     try {
       return bigInteger.shortValueExact();
-    } catch (ArithmeticException ex) {
+    } catch (ArithmeticException ignored) {
     }
     try {
       return bigInteger.intValueExact();
-    } catch (ArithmeticException ex) {
+    } catch (ArithmeticException ignored) {
     }
     try {
       return bigInteger.longValueExact();
-    } catch (ArithmeticException ex) {
+    } catch (ArithmeticException ignored) {
     }
     return bigInteger;
   }
