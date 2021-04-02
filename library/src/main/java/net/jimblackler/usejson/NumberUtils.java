59a7fdadbbadc1a852e858d01124b307c91fb910
Jim Blackler <jimblackler@google.com>
2021-04-02 10:54:36 +0100

    Don't rely on a Java 1.8 feature during number conversion.
    Increases compatibility, e.g. for Anrdoid.

--- library/src/main/java/net/jimblackler/usejson/NumberUtils.java
+++ library/src/main/java/net/jimblackler/usejson/NumberUtils.java
@@ -4,21 +4,24 @@ import java.math.BigInteger;
 
 public class NumberUtils {
   public static Object toBestObject(BigInteger bigInteger) {
-    try {
-      return bigInteger.byteValueExact();
-    } catch (ArithmeticException ignored) {
+    if (bigInteger.compareTo(BigInteger.valueOf(Byte.MIN_VALUE)) >= 0
+        && bigInteger.compareTo(BigInteger.valueOf(Byte.MAX_VALUE)) <= 0) {
+      return bigInteger.byteValue();
     }
-    try {
-      return bigInteger.shortValueExact();
-    } catch (ArithmeticException ignored) {
+
+    if (bigInteger.compareTo(BigInteger.valueOf(Short.MIN_VALUE)) >= 0
+        && bigInteger.compareTo(BigInteger.valueOf(Short.MAX_VALUE)) <= 0) {
+      return bigInteger.shortValue();
     }
-    try {
-      return bigInteger.intValueExact();
-    } catch (ArithmeticException ignored) {
+
+    if (bigInteger.compareTo(BigInteger.valueOf(Integer.MIN_VALUE)) >= 0
+        && bigInteger.compareTo(BigInteger.valueOf(Integer.MAX_VALUE)) <= 0) {
+      return bigInteger.intValue();
     }
-    try {
-      return bigInteger.longValueExact();
-    } catch (ArithmeticException ignored) {
+
+    if (bigInteger.compareTo(BigInteger.valueOf(Long.MIN_VALUE)) >= 0
+        && bigInteger.compareTo(BigInteger.valueOf(Long.MAX_VALUE)) <= 0) {
+      return bigInteger.longValue();
     }
     return bigInteger;
   }
